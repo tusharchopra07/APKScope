0e4f7ed97784b7df577f7ea091ae4552bf5ae1fd
Tushar Chopra <128215171+tusharchopra07@users.noreply.github.com>
2026-07-07 13:35:57 +0530

    Implement Update function for Model in app package

---
+++ internal/app/update.go
@@ -0,0 +1,21 @@
+package app
+
+import tea "github.com/charmbracelet/bubbletea"
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.Width = msg.Width
+		m.Height = msg.Height
+
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
